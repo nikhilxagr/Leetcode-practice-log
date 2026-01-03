58fdd0a3b0d09d638c7f9e0f3838baf55e5b307c
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-01-03 21:03:17 +0530

    Add solution for Q-58  Length of Last Word problem
    Implements a Python class with a method to return the length of the last word in a given string, handling extra spaces and providing example cases.

---
+++ STRING/Easy/0058_Length of Last Word.py
@@ -0,0 +1,46 @@
+# 58. Length of Last Word
+
+# Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+# A word is a maximal substring consisting of non-space characters only.
+
+
+# Example
+
+"""
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+Example 3:
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+"""
+
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        words = s.split()   
+        
+        last_word = words[-1]      
+        length = len(last_word)    
+        
+        return length    
+  
+    
+# Split = s.split() divides the string into a list of words based on spaces.
+    
+    
+# words[-1] accesses the last word in the list.
+    
+    
+# len(last_word) calculates the length of the last word.
+    
+    
