2a39c9c94e928b9381b97bb3f4de76696bf0963d
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-01-07 22:03:48 +0530

    Add solution for LeetCode Q-283 Move Zeroes
    Implements an in-place algorithm to move all zeroes in an array to the end while maintaining the order of non-zero elements.

---
+++ ARRAY/EASY/0283_Move Zeroes.py
@@ -0,0 +1,33 @@
+# 283. Move Zeroes
+
+# Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+# Note that you must do this in-place without making a copy of the array.
+
+# Example
+
+"""
+Example 1:
+
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+
+"""
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        index = 0
+        
+        for num in nums:
+            if num != 0:
+                nums[index] = num
+                index += 1
+        
+        while index < len(nums):
+            nums[index] = 0
+            index += 1
+            
