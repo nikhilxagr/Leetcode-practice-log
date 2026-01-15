82278db5484ea27e01a750fdea82b5252764b5ad
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-01-15 22:31:20 +0530

    Add solution for q 2108 - finding first palindromic string
    Implements a function to return the first palindromic string in a list of words, or an empty string if none exist. Includes example usage and problem description.

---
+++ SUPER EASY PROBLEMS/2108. Find First Palindromic String in the Array.py
@@ -0,0 +1,48 @@
+# 2108. Find First Palindromic String in the Array
+
+# Given an array of strings words, return the first palindromic string in the array. If there is no such string, return an empty string "".
+
+# A string is palindromic if it reads the same forward and backward.
+
+# Example
+
+"""
+
+Example 1:
+
+Input: words = ["abc","car","ada","racecar","cool"]
+Output: "ada"
+Explanation: The first string that is palindromic is "ada".
+Note that "racecar" is also palindromic, but it is not the first.
+
+Example 2:
+
+Input: words = ["notapalindrome","racecar"]
+Output: "racecar"
+Explanation: The first and only string that is palindromic is "racecar".
+
+Example 3:
+
+Input: words = ["def","ghi"]
+Output: ""
+Explanation: There are no palindromic strings, so the empty string is returned.
+
+"""
+
+# Solution:
+
+class Solution(object):
+    def firstPalindrome(self, words):
+        """
+        :type words: List[str]
+        :rtype: str
+        """
+        for word in words:
+            if word == word[::-1]:
+                return word
+        return ""
+    
+sol = Solution()
+print(sol.firstPalindrome(["abc","car","ada","racecar","cool"]))
+
+
