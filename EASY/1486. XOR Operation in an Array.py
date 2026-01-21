ae1530789bc01586ada6397fe27fcb290f2d761f
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-01-21 22:23:56 +0530

    Add solution for LeetCode 1486 XOR Operation in an Array
    Implements the xorOperation method to compute the bitwise XOR of an array defined by the problem statement.

---
+++ EASY/1486. XOR Operation in an Array.py
@@ -0,0 +1,37 @@
+# 1486. XOR Operation in an Array
+
+# You are given an integer n and an integer start.
+
+# Define an array nums where nums[i] = start + 2 * i (0-indexed) and n == nums.length.
+
+# Return the bitwise XOR of all elements of nums.
+
+# Examples
+
+"""
+Example 1:
+
+Input: n = 5, start = 0
+Output: 8
+Explanation: Array nums is equal to [0, 2, 4, 6, 8] where (0 ^ 2 ^ 4 ^ 6 ^ 8) = 8.
+Where "^" corresponds to bitwise XOR operator.
+Example 2:
+
+Input: n = 4, start = 3
+Output: 8
+Explanation: Array nums is equal to [3, 5, 7, 9] where (3 ^ 5 ^ 7 ^ 9) = 8.
+ """
+ 
+#  Solution:
+
+class Solution(object):
+    def xorOperation(self, n, start):
+        """
+        :type n: int
+        :type start: int
+        :rtype: int
+        """
+        ans = 0
+        for i in range(n):
+            ans ^= start + 2 * i
+        return ans
