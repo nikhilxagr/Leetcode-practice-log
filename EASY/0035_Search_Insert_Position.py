1caab43d8bd50d2c01587ab1823f0e3b99989e9b
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-01-26 17:34:33 +0530

    Refactor searchInsert into Solution class
    Moved the searchInsert function into a Solution class for better structure and encapsulation. Commented out previous standalone code and added example usage variables at the end.

--- EASY/0035_Search_Insert_Position.py
+++ EASY/0035_Search_Insert_Position.py
@@ -21,12 +21,24 @@ Output: 4
  """
  
  
-nums = [1,3,5,6]
-target = 5 
+# nums = [1,3,5,6]
+# target = 5 
  
-def searchInsert(self, nums, target):
-    for i in range(len(nums)):
-        if nums[i] >= target:
-          return i
-    return len(nums)
+# def searchInsert(self, nums, target):
+#     for i in range(len(nums)):
+#         if nums[i] >= target:
+#           return i
+#     return len(nums)
         
+        
+        
+class Solution(object):
+    def searchInsert(self, nums, target):
+        
+        for i in range(len(nums)):
+            if nums[i] >= target:
+                return i
+            return len(nums)
+        
+nums = [1,3,5,6]
+target = 5  
