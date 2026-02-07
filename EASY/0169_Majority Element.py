9152b3118280493cf4a78bce61cbfd07949c4f9f
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-02-07 23:42:49 +0530

    add soln of 169  mejority element
    Replace the in-loop early return with a full frequency count and return the element with the highest frequency using max(count, key=count.get).

--- EASY/0169_Majority Element.py
+++ EASY/0169_Majority Element.py
@@ -18,6 +18,24 @@ Output: 2
 
 """
 
+# class Solution(object):
+#     def majorityElement(self, nums):
+#         """
+#         :type nums: List[int]
+#         :rtype: int
+#         """
+#         count = {}
+#         for num in nums:
+#             if num in count:
+#                 count[num] += 1
+#             else:
+#                 count[num] = 1
+#             if count[num] > len(nums) // 2:
+#                 return num
+            
+            
+#Another Approach  By Maximum Occurrence    
+            
 class Solution(object):
     def majorityElement(self, nums):
         """
@@ -25,10 +43,10 @@ class Solution(object):
         :rtype: int
         """
         count = {}
+        
         for num in nums:
             if num in count:
                 count[num] += 1
             else:
                 count[num] = 1
-            if count[num] > len(nums) // 2:
-                return num
+        return max(count, key=count.get)
