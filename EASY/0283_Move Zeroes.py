d50f746695bcf320f54a724ca5d1393a3e88df75
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-02-15 23:44:51 +0530

    Refactor moveZeroes to use partition lists
    Replace the original two-pointer in-place algorithm with a clearer partition approach: collect non-zero elements into arr1 and zeros into arr2, then reassign nums[:] = arr1 + arr2. A commented alternative two-pointer implementation was added above, and some docstring/comment text was tweaked. The method still mutates the input list in-place and now returns nums.

--- EASY/0283_Move Zeroes.py
+++ EASY/0283_Move Zeroes.py
@@ -1,6 +1,4 @@
-# 283. Move Zeroes
 
-# Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
 
 # Note that you must do this in-place without making a copy of the array.
 
@@ -18,17 +16,32 @@ Output: [0]
 
 """
 
+# class Solution(object):
+#     def Zeroes(self, nums):
+#         index = 0
+        
+#         for num in nums:
+#             if num != 0:
+#                 nums[index] = num
+#                 index += 1
+        
+#         while index < len(nums):
+#             nums[index] = 0
+#             index += 1
+            
+                  
 class Solution(object):
     def moveZeroes(self, nums):
-        index = 0
+        arr1 =[]
+        arr2 = []
         
         for num in nums:
+
             if num != 0:
-                nums[index] = num
-                index += 1
-        
-        while index < len(nums):
-            nums[index] = 0
-            index += 1
-            
+                arr1.append(num)
+            else :
+                arr2.append(num)
+        nums[:]= arr1 + arr2
+        return nums            
+                                        
                   
