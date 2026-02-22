c4b9b1a98409a9d5aa619f3495683df635fbdce3
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-02-22 23:26:11 +0530

    Add soln for 33. Search in Rotated Sorted Array
     implementation that returns the index of target or -1. Note: the current implementation is O(n) though the problem requires O(log n) and should be replaced with a rotated-array binary search for production.

---
+++ MEDIUM/0033. Search in Rotated Sorted Array.py
@@ -0,0 +1,48 @@
+# 33. Search in Rotated Sorted Array
+
+# There is an integer array nums sorted in ascending order (with distinct values).
+
+# Prior to being passed to your function, nums is possibly left rotated at an unknown index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be left rotated by 3 indices and become [4,5,6,7,0,1,2].
+
+# Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+#  Example 1:
+
+# Input: nums = [4,5,6,7,0,1,2], target = 0
+# Output: 4
+# Example 2:
+
+# Input: nums = [4,5,6,7,0,1,2], target = 3
+# Output: -1
+# Example 3:
+
+# Input: nums = [1], target = 0
+# Output: -1
+
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        ans = -1
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return i
+      
+        return ans
+
+
+# nums = [4,5,6,7,0,1,2]
+# target = 9
+
+# ans = -1
+# for i in range(len(nums)):
+#     if nums[i] == target:
+#         print(i)
+#         break
+# else:    
+#     print(-1)
