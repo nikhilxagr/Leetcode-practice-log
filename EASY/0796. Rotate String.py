c316ef037b769aee24e6a784820c003c0f8dd620
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-02-26 19:38:50 +0530

    Add solution for LeetCode 796 Rotate String
     Implements Solution.rotateString in EASY/0796. Rotate String.py: it first checks length equality and then uses the standard concatenation trick (s in goal+goal) to determine if goal is a rotation of s. This yields O(n) time and O(n) extra space.

---
+++ EASY/0796. Rotate String.py
@@ -0,0 +1,30 @@
+# 796. Rotate String
+
+# Given two strings s and goal, return true if and only if s can become goal after some number of shifts on s.
+
+# A shift on s consists of moving the leftmost character of s to the rightmost position.
+
+# For example, if s = "abcde", then it will be "bcdea" after one shift.
+ 
+
+# Example 1:
+
+# Input: s = "abcde", goal = "cdeab"
+# Output: true
+# Example 2:
+
+# Input: s = "abcde", goal = "abced"
+# Output: false
+
+
+class Solution(object):
+    def rotateString(self, s, goal):
+        """
+        :type s: str
+        :type goal: str
+        :rtype: bool
+        """
+        if len(s) != len(goal):
+            return False
+
+        return s in goal + goal
