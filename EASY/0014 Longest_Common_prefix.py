1db1661d7f61585a5d99ee30c829b12acfa8749e
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-03-10 22:40:39 +0530

    Refactor longestCommonPrefix implementation
    first string to a variable, iterate characters from it and compare against all strings, and return early on mismatch.

--- EASY/0014 Longest_Common_prefix.py
+++ EASY/0014 Longest_Common_prefix.py
@@ -1,46 +1,25 @@
 # 14. Longest Common Prefix
-
+# Revised
 # Write a function to find the longest common prefix string amongst an array of strings.
 
 # If there is no common prefix, return an empty string "".
 
 class Solution(object):
     def longestCommonPrefix(self, strs):
-         """
+        """
         :type strs: List[str]
         :rtype: str
-         """
-         prefix = ""
-         for i in range(len(strs[0])):
-             
-            char = strs[0][i]
-            for s in strs[1:]:
+        """
+        prefix = ""
+        first = strs[0]
+
+        for i in range(len(first)):
+            char = first[i]
+
+            for s in strs:
                 if i >= len(s) or s[i] != char:
                     return prefix
-                
+
             prefix += char
-         return prefix
- 
-           
-           
-# Solve by another approach
-           
-#   class Solution(object):
-#     def longestCommonPrefix(self, strs):
-#         """
-#         :type strs: List[str]
-#         :rtype: str
-#         """
-#         first = strs[0]
-#         prefix = ""
-#         print(first)
-#         for i in range(0, len(first)):
-#             for j in range(1, len(strs)):
-#                 print("1. ", str(first[:i]))
-#                 print("2. ", str(strs[j]))
-#                 currPre = str(first[:(i + 1)])
-#                 thisStr = str(strs[j])[:(i+1)]
-#                 if currPre != thisStr:
-#                     return prefix
-#             prefix = prefix + first[i]
-#         return prefix             
+
+        return prefix
