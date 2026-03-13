4a60a98934894075268606de78bd1fc0950f1ba2
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-03-13 23:47:38 +0530

    revised the problem 35  and 39

--- EASY/0035_Search_Insert_Position.py
+++ EASY/0035_Search_Insert_Position.py
@@ -1,5 +1,6 @@
 
 # 35. Search Insert Position
+#revised
 
 """
  Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
@@ -39,6 +40,4 @@ class Solution(object):
             if nums[i] >= target:
                 return i
             return len(nums)
-        
-nums = [1,3,5,6]
-target = 5  
+         
