962fa47b509108c25b28d13662c3c7e4ac62ff74
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-03-14 23:28:26 +0530

    Mark Length of Last Word solution as revised
    Add a '#revised' comment to EASY/0058_Length of Last Word.py to indicate the file was updated. This is a non-functional change for tracking; no logic was modified.

--- EASY/0058_Length of Last Word.py
+++ EASY/0058_Length of Last Word.py
@@ -1,5 +1,5 @@
 # 58. Length of Last Word
-
+#revised
 # Given a string s consisting of words and spaces, return the length of the last word in the string.
 
 # A word is a maximal substring consisting of non-space characters only.
