77480fafd339f73c83349bbd4aee34075a57d870
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-03-16 23:45:15 +0530

    Add algorithm explanation and author note
    Add a brief author note and a step-by-step algorithm comment describing the approach to remove duplicates from a sorted linked list.

--- EASY/0083. Remove Duplicates from Sorted List.py
+++ EASY/0083. Remove Duplicates from Sorted List.py
@@ -1,5 +1,5 @@
 # 83. Remove Duplicates from Sorted List
-
+#revised by myself
 # Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
 
 # Examples:
@@ -41,3 +41,10 @@ print(sol.deleteDuplicates([1,1,2]))
 print(sol.deleteDuplicates([1,1,2,3,3])) 
         
         
+# Algprithm:
+# 1. Check if the head is None, if it is, return None.
+# 2. Initialize a variable current to the head of the linked list.
+# 3. While current is not None and current.next is not None:
+#    a. If the value of current is equal to the value of current.next, it means we have found a duplicate. In this case, we skip the next node by setting current.next to current.next.next.
+#    b. If the value of current is not equal to the value of current.next, it means we have found a unique element. In this case, we move the current pointer to the next node.
+# 4. After the loop, we return the head of the modified linked list, which now contains only unique elements.
