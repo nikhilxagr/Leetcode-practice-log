b4c39682de4bdc138f24e559a3139def5f90f124
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-03-29 23:50:07 +0530

    add the revised 412. Fizz Buzz problem

--- EASY/0412 Fizz Buzz.py
+++ EASY/0412 Fizz Buzz.py
@@ -1,5 +1,5 @@
 # Given an integer n, return a string array answer (1-indexed) where:
-
+# revised 
 # answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
 # answer[i] == "Fizz" if i is divisible by 3.
 # answer[i] == "Buzz" if i is divisible by 5.
@@ -38,4 +38,13 @@ class Solution(object):
                 ans.append(str(i))   
         return ans                                                
 
-        
+# Algorithm:
+# 1. Create an empty list ans to store the results.
+# 2. Loop through the numbers from 1 to n (inclusive).
+# 3. For each number i, check the following conditions:
+#    a. If i is divisible by both 3 and 5, append "FizzBuzz" to ans.
+#    b. If i is divisible by 3, append "Fizz" to ans.
+#    c. If i is divisible by 5, append "Buzz" to ans.
+#    d. If none of the above conditions are true, append the string representation of i
+#       to ans.
+# 4. After the loop, return the ans list containing the results.
