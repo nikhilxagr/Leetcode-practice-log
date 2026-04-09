36c8cb6cb0812feb61491e878ec6671ba69dee16
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-04-09 23:21:30 +0530

    Add algorithm comments and minor revisions
    Add a "# revised" marker and an explicit Algorithm comment block explaining scanning from the end for the first odd digit and returning the prefix up to that index. Also ensure file ends with a newline and adjust minor formatting; no logic changes to the solution.

--- EASY/1903. Largest Odd Number in String.py
+++ EASY/1903. Largest Odd Number in String.py
@@ -1,5 +1,7 @@
 # 1903. Largest Odd Number in String
 
+# revised 
+
 # You are given a string num, representing a large integer. Return the largest-valued odd integer (as a string) that is a non-empty substring of num, or an empty string "" if no odd integer exists.
 
 # A substring is a contiguous sequence of characters within a string.
@@ -35,3 +37,9 @@ class Solution(object):
                 return num[:i+1]
             
         return ""
+    
+# Algorithm:
+# 1. We will iterate through the string num from the end to the beginning.
+# 2. For each character, we will check if it is an odd digit (i.e., if the integer value of the character modulo 2 is equal to 1).   
+# 3. If we find an odd digit, we will return the substring of num from the beginning to the current index (inclusive).
+# 4. If we finish iterating through the string without finding any odd digit, we will return an empty string "".
