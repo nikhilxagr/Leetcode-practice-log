829667763510280cb2efc34fcd9c61bf6fd13259
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-04-25 21:45:37 +0530

    added solution for 79. Word Search.py

---
+++ MEDIUM/79. Word Search.py
@@ -0,0 +1,52 @@
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Output: true
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+Output: true
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+Output: false
+
+
+class Solution(object):
+    def exist(self, board, word):
+        rows = len(board)
+        cols = len(board[0])
+
+        def dfs(r, c, index):
+            
+            if index == len(word):
+                return True
+
+            
+            if r < 0 or c < 0 or r >= rows or c >= cols:
+                return False
+            if board[r][c] != word[index]:
+                return False
+
+           
+            temp = board[r][c]
+            board[r][c] = "#"
+
+           
+            found = (dfs(r+1, c, index+1) or
+                     dfs(r-1, c, index+1) or
+                     dfs(r, c+1, index+1) or
+                     dfs(r, c-1, index+1))
+
+           
+            board[r][c] = temp
+
+            return found
+
+        
+        for i in range(rows):
+            for j in range(cols):
+                if dfs(i, j, 0):
+                    return True
+
+        return False
