05591677e583195db4fec0d19efcf82bef3e678d
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-05-04 22:00:34 +0530

    revised 451. Sort Characters By Frequency
    Algorithm:
    1. We use the Counter class from the collections module to count the frequency of each character in the input string s.
    2. We then use the most_common() method of the Counter object to get a list of tuples, where each tuple contains a character and its corresponding frequency, sorted in decreasing order of frequency.
    3. Finally, we construct the output string by repeating each character according to its frequency and joining them together using the join() method. The resulting string is returned as the output.

--- MEDIUM/0451. Sort Characters By Frequency.py
+++ MEDIUM/0451. Sort Characters By Frequency.py
@@ -1,4 +1,5 @@
 # 451. Sort Characters By Frequency
+#revised
 
 # Given a string s, sort it in decreasing order based on the frequency of the characters. The frequency of a character is the number of times it appears in the string.
 
@@ -31,3 +32,8 @@ class Solution(object):
         from collections import Counter
         
         return ''.join(ch * freq for ch, freq in Counter(s).most_common())
+    
+# Algorithm:
+# 1. We use the Counter class from the collections module to count the frequency of each character in the input string s.
+# 2. We then use the most_common() method of the Counter object to get a list of tuples, where each tuple contains a character and its corresponding frequency, sorted in decreasing order of frequency.
+# 3. Finally, we construct the output string by repeating each character according to its frequency and joining them together using the join() method. The resulting string is returned as the output.
