98dd32868c922beafce2bb3b059e5ec4d10a6f38
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-06-10 17:38:43 +0530

    revised the 118. Pascal's Triangle
    Algorithm:
    1. Initialize an empty list called triangle to store the rows of Pascal's triangle.
    2. Loop through each row number from 0 to numRows - 1.
    3. For each row, create a list called row initialized with 1's, where the length of the row is row_num + 1.
    4. For each element in the row (except the first and last), calculate its value as the sum of the two elements directly above it from the previous row in the triangle.
    5. Append the completed row to the triangle list.

--- EASY/0118. Pascal's_Triangle.py
+++ EASY/0118. Pascal's_Triangle.py
@@ -1,5 +1,5 @@
 # 118. Pascal's Triangle
-
+# revised 
 #Revisit
 """
 Given an integer numRows, return the first numRows of Pascal's triangle.
@@ -40,3 +40,10 @@ class Solution(object):
             triangle.append(row)
 
         return triangle
+    
+# Algorithm:
+# 1. Initialize an empty list called triangle to store the rows of Pascal's triangle.
+# 2. Loop through each row number from 0 to numRows - 1.
+# 3. For each row, create a list called row initialized with 1's, where the length of the row is row_num + 1.
+# 4. For each element in the row (except the first and last), calculate its value as the sum of the two elements directly above it from the previous row in the triangle.
+# 5. Append the completed row to the triangle list.
