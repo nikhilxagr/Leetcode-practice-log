a486f0d159ddf74d52ad15544f3cce90bcc21f09
Nikhil Agrahari <nikhilgithub2005@gmail.com>
2026-06-20 15:40:54 +0530

    solve the sq of a sorted array using two pointes
    Approach - use two pointers technique to fill the result array from the end to the beginning.
    
    Compare the squares of the elements at the left and right pointers, and place the larger square at the current position in the result array.
    
    Move the corresponding pointer inward and repeat until all elements are processed. This approach has a time complexity of O(n) and does not require additional sorting.

--- EASY/977. Squares of a Sorted Array.py
+++ EASY/977. Squares of a Sorted Array.py
@@ -39,3 +39,38 @@ class Solution(object):
 #    a. Calculate the square of `num` using the `pow` function and append it to the `result` list.
 # 3. After the loop, sort the `result` list in non-decreasing order using the `sort` method.
 # 4. Return the sorted `result` list, which contains the squares of the numbers in non-decreasing order.
+
+# Another Approach:  Using Two Pointers Technique
+
+class Solution(object):
+    def sortedSquares(self, nums):
+        
+        n = len(nums)
+        result = [0] * n
+
+        i = 0
+        j = n - 1
+        pos = n - 1
+
+        while i <= j:
+            
+            left_sq = nums[i] * nums[i]
+            right_sq = nums[j] * nums[j]
+
+            if left_sq > right_sq:
+                result[pos] = left_sq
+                i += 1
+                
+            else:
+                result[pos] = right_sq
+                j -= 1
+
+            pos -= 1
+
+        return result
+    
+# Approach - use two pointers technique to fill the result array from the end to the beginning.
+
+# Compare the squares of the elements at the left and right pointers, and place the larger square at the current position in the result array.
+
+# Move the corresponding pointer inward and repeat until all elements are processed. This approach has a time complexity of O(n) and does not require additional sorting.
